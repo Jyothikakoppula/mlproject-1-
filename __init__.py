4b8b1aa7842e90eee3254fc1c172f1a44100cf0c
Jyothikakoppula <93107489+Jyothikakoppula@users.noreply.github.com>
2023-11-05 05:00:23 +0530

    Add files via upload

--- __init__.py
+++ __init__.py
@@ -1,1 +1,8 @@
-from corsheaders.checks import check_settings  # noqa: F401
+# -*- coding: utf-8 -*-
+try:
+    from ._version import version as __version__
+except ImportError:
+    __version__ = 'unknown'
+
+__all__ = ['easter', 'parser', 'relativedelta', 'rrule', 'tz',
+           'utils', 'zoneinfo']
